24ba4c23d73a340af64e6281c99d9e0e8f020bbb
Rajat09Sharma <rajat9.lyfe@gmail.com>
2024-09-30 15:08:55 +0530

    mainnavigation file and style sheet are changed

--- src/component/MainNavigation.jsx
+++ src/component/MainNavigation.jsx
@@ -1,9 +1,57 @@
 
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function MainNavigation() {
 
+    const [isNavlinkCollapsed, setNavlinkCollapsed] = useState(false)
 
+    function handleNavlinksCollapse() {
+        setNavlinkCollapsed((prevs) => {
+            return !prevs;
+        })
+    }
+
+    useEffect(() => {
+        let size = window.innerWidth;
+        let navLinks = document.querySelectorAll(".toggle");
+
+        const updateWindowDimensions = () => {
+            size = window.innerWidth;
+        };
+        console.log(size);
+        if (size > 992) {
+            navLinks.forEach((link) => {
+                link.removeAttribute("data-toggle");
+                link.removeAttribute("data-target");
+            });
+            console.log("removed attributes");
+        } else {
+            navLinks.forEach((link) => {
+                link.setAttribute("data-toggle", "collapse");
+                link.setAttribute("data-target", "#navbarTogglerDemo02");
+            });
+            console.log("set attributes");
+        }
+        
+        window.addEventListener("resize", () => {
+            updateWindowDimensions();
+            if (size > 992) {
+                navLinks.forEach((link) => {
+                    link.removeAttribute("data-toggle");
+                    link.removeAttribute("data-target");
+                });
+                console.log("removed attributes");
+            } else {
+                navLinks.forEach((link) => {
+                    link.setAttribute("data-toggle", "collapse");
+                    link.setAttribute("data-target", "#navbarTogglerDemo02");
+                });
+                console.log("set attributes");
+            }
+
+        });
+    }, [isNavlinkCollapsed])
     return (
         <>
             <nav className="navbar navbar-expand-lg sticky-top bg-dark" data-bs-theme="dark">
@@ -15,17 +63,17 @@ export default function MainNavigation() {
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                         <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} aria-current="page" to="/">Home</NavLink>
+                                <NavLink className={({ isActive }) => isActive ? "nav-link active toggle" : "nav-link toggle active"} aria-current="page" to="/" onClick={handleNavlinksCollapse} >Home</NavLink>
                             </li>
                             <li className="nav-item dropdown">
                                 <NavLink className={({ isActive }) => isActive ? "nav-link dropdown-toggle active" : "nav-link dropdown-toggle"} role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     About Us
                                 </NavLink>
                                 <ul className="dropdown-menu">
-                                    <li><NavLink className="dropdown-item" to="/about-us">About SPS</NavLink></li>
-                                    <li><NavLink className="dropdown-item" to="/principal-message">Principal&#39;s Message</NavLink></li>
+                                    <li><NavLink className="dropdown-item toggle" to="/about-us" onClick={handleNavlinksCollapse}>About SPS</NavLink></li>
+                                    <li><NavLink className="dropdown-item toggle" to="/principal-message" onClick={handleNavlinksCollapse}>Principal&#39;s Message</NavLink></li>
                                     <li><hr className="dropdown-divider" /></li>
-                                    <li><NavLink className="dropdown-item" to="/achievements">Our Achivements</NavLink></li>
+                                    <li><NavLink className="dropdown-item toggle" to="/achievements" onClick={handleNavlinksCollapse}>Our Achivements</NavLink></li>
                                 </ul>
                             </li>
                             <li className="nav-item dropdown">
@@ -33,10 +81,10 @@ export default function MainNavigation() {
                                     Curriculum
                                 </NavLink>
                                 <ul className="dropdown-menu">
-                                    <li>  <NavLink className="dropdown-item" to="/primary-curriculum">Primary Curriculum (Grades 1-5)</NavLink></li>
-                                    <li> <NavLink className="dropdown-item" to="/secondary-curriculum">Secondary (Grades 6-10)</NavLink></li>
+                                    <li>  <NavLink className="dropdown-item toggle" to="/primary-curriculum" onClick={handleNavlinksCollapse}>Primary Curriculum (Grades 1-5)</NavLink></li>
+                                    <li> <NavLink className="dropdown-item toggle" to="/secondary-curriculum" onClick={handleNavlinksCollapse}>Secondary (Grades 6-10)</NavLink></li>
                                     <li><hr className="dropdown-divider" /></li>
-                                    <li> <NavLink className="dropdown-item" to="/Senior-curriculum">Senior Secondary (Grades 11-12)</NavLink></li>
+                                    <li> <NavLink className="dropdown-item toggle" to="/Senior-curriculum" onClick={handleNavlinksCollapse}>Senior Secondary (Grades 11-12)</NavLink></li>
                                 </ul>
                             </li>
                             <li className="nav-item dropdown">
@@ -44,7 +92,7 @@ export default function MainNavigation() {
                                     Academic
                                 </NavLink>
                                 <ul className="dropdown-menu">
-                                    <li>  <NavLink className="dropdown-item" to="/methodolog">Teaching Methodologies</NavLink></li>
+                                    <li>  <NavLink className="dropdown-item toggle" to="/methodolog" onClick={handleNavlinksCollapse}>Teaching Methodologies</NavLink></li>
                                 </ul>
                             </li>
                             <li className="nav-item dropdown">
@@ -52,27 +100,27 @@ export default function MainNavigation() {
                                     Admissions
                                 </NavLink>
                                 <ul className="dropdown-menu">
-                                    <li>  <NavLink className="dropdown-item" to="/process">Process</NavLink></li>
-                                    <li> <NavLink className="dropdown-item" to="/criteria">Criteria</NavLink></li>
+                                    <li>  <NavLink className="dropdown-item toggle" to="/process" onClick={handleNavlinksCollapse}>Process</NavLink></li>
+                                    <li> <NavLink className="dropdown-item toggle" to="/criteria" onClick={handleNavlinksCollapse}>Criteria</NavLink></li>
                                 </ul>
                             </li>
                             <li className="nav-item">
-                                <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link active"} to="/faculty">Faculty</NavLink>
+                                <NavLink className={({ isActive }) => isActive ? "nav-link active toggle" : "nav-link active toggle"} to="/faculty" onClick={handleNavlinksCollapse}>Faculty</NavLink>
                             </li>
                             <li className="nav-item dropdown">
                                 <NavLink className={({ isActive }) => isActive ? "nav-link dropdown-toggle active" : "nav-link dropdown-toggle"} role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Students
                                 </NavLink>
                                 <ul className="dropdown-menu">
-                                    <li>  <NavLink className="dropdown-item" to="/sps-life">Life at Springdale</NavLink></li>
-                                    <li> <NavLink className="dropdown-item" to="/student-council">Student Council</NavLink></li>
+                                    <li>  <NavLink className="dropdown-item toggle" to="/sps-life" onClick={handleNavlinksCollapse}>Life at Springdale</NavLink></li>
+                                    <li> <NavLink className="dropdown-item toggle" to="/student-council" onClick={handleNavlinksCollapse}>Student Council</NavLink></li>
                                 </ul>
                             </li>
                             <li className="nav-item">
-                                <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link active"} to="/gallery">Gallery</NavLink>
+                                <NavLink className={({ isActive }) => isActive ? "nav-link active toggle" : "nav-link active toggle"} to="/gallery" onClick={handleNavlinksCollapse}>Gallery</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link active"} to="/contact-us">Contact Us</NavLink>
+                                <NavLink className={({ isActive }) => isActive ? "nav-link active toggle" : "nav-link active toggle"} to="/contact-us" onClick={handleNavlinksCollapse}>Contact Us</NavLink>
                             </li>
                         </ul>
                     </div>
